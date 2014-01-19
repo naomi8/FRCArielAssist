706c873a29a5833be62202393f2b8282baf6e8fd
primetoxinz <primetoxinzz@gmail.com>
2014-01-18 21:23:32 -0500

    Added Robot Drive
    Got this from the email

--- RobotTemplate/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
+++ RobotTemplate/src/edu/wpi/first/wpilibj/templates/RobotTemplate.java
@@ -1,5 +1,7 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* class THUG LYFE ACTIVATED:).
+ * Have fun!
+ * Copyright (c) FIRST 2008. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -7,9 +9,10 @@
 
 package edu.wpi.first.wpilibj.templates;
 
-
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SimpleRobot;
-
+import edu.wpi.first.wpilibj.Timer;
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the SimpleRobot
@@ -18,18 +21,41 @@ import edu.wpi.first.wpilibj.SimpleRobot;
  * directory.
  */
 public class RobotTemplate extends SimpleRobot {
-    /**
-     * This function is called once each time the robot enters autonomous mode.
+    RobotDrive drive = new RobotDrive(1,2);
+    //left channel = 1, right channel = 2
+    Joystick leftStick = new Joystick(1);
+    Joystick rightStick = new Joystick(1);
+    /*
+     * This function is called once each time th    e robot enters autonomous mode.
      */
-    public void autonomous() {
-        
+    public RobotTemplate() {
+        getWatchdog().setEnabled(false);
     }
+    
+    public void autonomous() {
+        getWatchdog().setEnabled(true);
+        getWatchdog().setExpiration(0.5);
+        drive.drive(1,0);
+        Timer.delay(2);
+        drive.drive(0,0);
+        for (int i = 0; i < 4; i++) {
+            drive.drive(0.5,0.0);
+            Timer.delay(2.0);
+            drive.drive(0.0,0.75);
+        }
+        drive.drive(0.0,0.0);
+   }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-
+            while (true && isOperatorControl() && isEnabled())
+            {
+                drive.tankDrive(leftStick,rightStick);
+                drive.setLeftRightMotorOutputs(.5,.5);
+                Timer.delay(0.005);
+            }
     }
     
     /**
@@ -38,4 +64,6 @@ public class RobotTemplate extends SimpleRobot {
     public void test() {
     
     }
+    
 }
+    
